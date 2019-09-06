740784e9871ee7534106e127dd09fad58b039345
Megan <mennis0145@gmail.com>
2019-09-06 08:57:34 +0100

    Axios GET request responding, function to iterate over data returned & pass it into tabCreator function to create component, append each to DOM

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then( response => {
+    response.data.topics.forEach( topic => {
+        topicsContainer.appendChild(tabCreator(topic));
+    });
+})
+.catch(error => {
+    debugger
+});
+
+const topicsContainer = document.querySelector('.topics');
+
+function tabCreator(title) {
+    const topicTitle = document.createElement('div');
+    topicTitle.classList.add('tab');
+    topicTitle.textContent = title;
+
+    return topicTitle;
+}
