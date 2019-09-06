7ece0877686c06102d38e315c092f8fc422f9c3c
Megan <mennis0145@gmail.com>
2019-09-06 08:39:06 +0100

    Header component created and new header appended to DOM

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,36 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+
+//Wasn't sure whether to set the textContent of the date, heading and temperature to 
+//fixed values in example above WITHIN my component, or to have my component accept
+//variable data as parameter
+//In the end, declared variable newHeading and passed it into component
+
+function Header(headerSettings) {
+
+const {date, heading, temp} = headerSettings;
+
+const header = document.createElement('div');
+header.classList.add('header');
+
+const headerDate = document.createElement('span');
+headerDate.classList.add('date');
+headerDate.textContent = date;
+
+const headerH1 = document.createElement('h1');
+headerH1.textContent = heading;
+
+const headerTemp = document.createElement('span');
+headerTemp.classList.add('temp');
+headerTemp.textContent  = temp;
+
+header.append(headerDate, headerH1, headerTemp);
+
+return header;
+
+}
+
+const headerContainer = document.querySelector('.header-container');
+const newHeader = {date:'Friday September 6', heading:'Lambda Times', temp:'98°'};
+headerContainer.appendChild(Header(newHeader));
